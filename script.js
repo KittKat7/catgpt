5a240a9a948dd781bacb427d760ac8b2c2166284
--global <rainbowtabby7@gmail.com>
2023-04-06 12:43:24 -0400

    Added random delay to printing output.

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const sendButton = document.querySelector('#send-button');
 const greetingMessages = ['Hello!', 'Hi there!', 'Hey, what\'s up?'];
 
 sendButton.addEventListener('click', () => {
-    gotMessage();
+	gotMessage();
 });
 
 messageInput.addEventListener("keydown", function(event) {
-    if (event.keyCode === 13) {
-        gotMessage();
-    }
+	if (event.keyCode === 13) {
+		gotMessage();
+	}
 });
 
 function gotMessage() {
@@ -19,25 +19,39 @@ function gotMessage() {
 	if (messageText) {
 		addMessage('sent', messageText);
 		setTimeout(() => {
-            //const randomIndex = Math.floor(Math.random() * greetingMessages.length);
-            //const responseText = greetingMessages[randomIndex];
+			//const randomIndex = Math.floor(Math.random() * greetingMessages.length);
+			//const responseText = greetingMessages[randomIndex];
 			const responseText = meows(messageText)
-            addMessage('received', responseText);
-        }, 1000);
-        messageInput.value = '';
-    }
+			addMessage('received', responseText);
+		}, 1000);
+		messageInput.value = '';
+	}
 }
 
 function addMessage(type, text) {
-    const messageClass = type === 'sent' ? 'sent' : 'received';
-    const message = document.createElement('li');
-    message.classList.add('message', messageClass);
-    const messageContent = document.createElement('div');
-    messageContent.classList.add('message-content');
-    messageContent.innerText = text;
-    message.appendChild(messageContent);
-	const chatDiv = document.querySelector('.chat-body');
-    messageList.appendChild(message);
+	const messageClass = type === 'sent' ? 'sent' : 'received';
+	const message = document.createElement('li');
+	message.classList.add('message', messageClass);
+	const messageContent = document.createElement('div');
+	messageContent.classList.add('message-content');
+	message.appendChild(messageContent);
+	//const chatDiv = document.querySelector('.chat-body');
+	messageList.appendChild(message);
+
+	let index = 0;
+	let addNextWord = function() {
+		if (index < text.length) {
+		messageContent.innerText += text[index] + ' ';
+		index++;
+		let timeMod = Math.floor(Math.random() * 5)
+		setTimeout(addNextWord, 100 * timeMod);
+		}
+	}
+	if (type == 'received') {
+		addNextWord();
+	} else {
+		messageContent.innerText = text;
+	}
 }
 
 function tokenize(text) {
@@ -49,41 +63,41 @@ function meows(text) {
 	let tokens = tokenize(text);
 	tokenCount = tokens.length;
 	counter = tokenCount * Math.floor(Math.random() * 8);
-	let output = "";
+	let output = [];
 	for (let i = 0; i < counter; i++) {
+		let word = '';
 		n = Math.floor(Math.random() * 10);
 		if (n % 5 != 0) {
-			output += 'm';
+			word += 'm';
 		}
 
 		n = Math.floor(Math.random() * 10);
 		if (n < 8) {
 			for (let j = 0; j < n/2; j++) {
-				output += 'e';
+				word += 'e';
 			}
 		}
 
 		n = Math.floor(Math.random() * 10);
 		if (n < 8) {
 			for (let j = 0; j < n/2; j++) {
-				output += 'o';
+				word += 'o';
 			}
 		}
 
 		n = Math.floor(Math.random() * 10);
 		if (n == 9) {
 			for (let j = 0; j < n/3; j++) {
-				output += 'e';
+				word += 'e';
 			}
 		}
 
 		n = Math.floor(Math.random() * 10);
 		if (n != 0) {
-			output += 'w';
+			word += 'w';
 		}
-
-		output += " ";
+		output.push(' ' + word);
 	}
-	output = output.charAt(0).toUpperCase() + output.substring(1);
+	output[0] = output[0].charAt(1).toUpperCase() + output[0].substring(2);
 	return output;
-}
+  }
